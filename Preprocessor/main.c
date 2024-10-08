e940b3d034d680b007332894fb33186b550e9469
keinoT480s <keinot480s@2001-14bb-c4-5ec-3828-5f05-a7dd-bc65.rev.dnainternet.fi>
2024-10-08 14:18:02 +0300

    added preprocessor

---
+++ Preprocessor/main.c
@@ -0,0 +1,17 @@
+
+/* C Variables */
+
+#include <stdio.h>
+#include <inttypes.h>
+
+enum boolean{FALSE=0, TRUE};
+enum retro_computer{ PET=1, VIC20, C64, C128, AMIGA};
+
+
+int main() {
+
+
+  printf("In main\n");
+
+  return 0;
+}
